ed1254e42a10e0dc54d47ad49bbb9c567ccd0578
Andreas Brett <andreasbrett@users.noreply.github.com>
2018-05-08 10:06:38 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,10 +3,13 @@
 import TelegramBot
 
 # create TelegramBot object
-tgb = TelegramBot.tgb("ENTER-YOUR-ACCESS-TOKEN", "ENTER-YOUR-GLOBAL-CHAT-ID-HERE")
+tgb_init = TelegramBot.tgb("ENTER-YOUR-ACCESS-TOKEN")
 
 # print latest updates (= all messages sent to it) to find out chat IDs
-tgb.getUpdates()
+tgb_init.getUpdates()
+
+# create TelegramBot object
+tgb = TelegramBot.tgb("ENTER-YOUR-ACCESS-TOKEN", "ENTER-YOUR-GLOBAL-CHAT-ID-HERE")
 
 # send message to (globally defined) chat
 tgb.sendMessage("Hey there!\nI'm your new bot.")
